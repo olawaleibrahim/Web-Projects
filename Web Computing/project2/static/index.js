b5d8e190bb296d148355a018a7b0300da1ce1426
OLAWALE O. I <ibrahim.olaawale13@gmail.com>
2020-05-31 21:32:38 +0100

    Channels, Alerts and Messages layouts

--- Web Computing/project2/static/index.js
+++ Web Computing/project2/static/index.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#create').disabled = true;
+    document.querySelector('#channel').onkeyup = () => {
+        if (document.querySelector('#channel').value.length > 0) 
+            document.querySelector('#create').disabled = false;
+        else
+            document.querySelector('#create').disabled = true;
+    
+        };
+    
+
+    });
+
+document.addEventListener('DOMContentLoaded', () => {
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
     socket.on('connect', () => {
         document.querySelector('#form').onsubmit = () => {
@@ -13,10 +26,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     socket.on('channel created', data => {
         const li = document.createElement('a');
+        li.className = "container"
         li.href = document.querySelector('#channel').value;
+        li.style.borderStyle = "double";
         li.innerHTML = `Channel ${data.selection}`;
         document.querySelector('#channelslink').append(li);
         const br = document.createElement('br');
         document.querySelector('#channelslink').append(br);
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#create').onclick = () => {
+        const alrt = document.createElement('div');
+        const channel = document.querySelector('#channel').value;
+        alrt.innerHTML = `${channel} succesfully created`;
+        alrt.className = 'alert alert-success'
+        document.querySelector('#alrt').append(alrt);
+    };
+});
+
